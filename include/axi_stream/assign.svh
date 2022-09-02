a0556d3ece42382c41da8a8fea5d6a74f86647c7
Tim Fischer <fischeti@iis.ee.ethz.ch>
2022-09-02 09:00:48 +0200

    Fix assign.svh and typedef.svh name swap

--- include/axi_stream/assign.svh
+++ include/axi_stream/assign.svh
@@ -2,75 +2,83 @@
 // Solderpad Hardware License, Version 0.51, see LICENSE for details.
 // SPDX-License-Identifier: SHL-0.51
 
-// Author: Noah Huetter <huettern@iis.ee.ethz.ch>
+// Authors:
+//  - Noah Huetter <huettern@iis.ee.ethz.ch>
+//  - Nils Wistoff <nwistoff@iis.ee.ethz.ch>
 
 // Macros to define AXI Stream Channel and Request/Response Structs
 
-`ifndef AXI_STREAM_TYPEDEF_SVH_
-`define AXI_STREAM_TYPEDEF_SVH_
+`ifndef AXI_STREAM_ASSIGN_SVH_
+`define AXI_STREAM_ASSIGN_SVH_
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////
-// AXI4-Stream Channel and Request/Response Structs
-`define AXI_STREAM_TYPEDEF_S_CHAN_T(s_chan_t, tdata_t, tstrb_t, tkeep_t, tlast_t, tid_t, tdest_t, tuser_t) \
-  typedef struct packed {                                                                                  \
-    tdata_t data;                                                                                          \
-    tstrb_t strb;                                                                                          \
-    tkeep_t keep;                                                                                          \
-    tlast_t last;                                                                                          \
-    tid_t id;                                                                                              \
-    tdest_t dest;                                                                                          \
-    tuser_t user;                                                                                          \
-  } s_chan_t;
-`define AXI_STREAM_TYPEDEF_REQ_T(req_stream_t, s_chan_t) \
-  typedef struct packed {                                \
-    s_chan_t            t;                               \
-    logic               tvalid;                          \
-  } req_stream_t;
-`define AXI_STREAM_TYPEDEF_RSP_T(resp_stream_t, tready_t) \
-  typedef struct packed {                                 \
-    tready_t             tready;                          \
-  } resp_stream_t;
+// Internal implementation for assigning one stream structs or interface to another struct or
+// interface.  The path to the signals on each side is defined by the `__sep*` arguments.  The
+// `__opt_as` argument allows to use this standalone (with `__opt_as = assign`) or in assignments
+// inside processes (with `__opt_as` void).
+`define __AXI_STREAM_TO_S(__opt_as, __lhs, __lhs_sep, __rhs, __rhs_sep) \
+  __opt_as __lhs``__lhs_sep``data   = __rhs``__rhs_sep``data;           \
+  __opt_as __lhs``__lhs_sep``strb   = __rhs``__rhs_sep``strb;           \
+  __opt_as __lhs``__lhs_sep``keep   = __rhs``__rhs_sep``keep;           \
+  __opt_as __lhs``__lhs_sep``last   = __rhs``__rhs_sep``last;           \
+  __opt_as __lhs``__lhs_sep``id     = __rhs``__rhs_sep``id;             \
+  __opt_as __lhs``__lhs_sep``user   = __rhs``__rhs_sep``user;           \
+  __opt_as __lhs``__lhs_sep``dest   = __rhs``__rhs_sep``dest;
+`define __AXI_STREAM_TO_REQ(__opt_as, __lhs, __lhs_sep, __rhs, __rhs_sep) \
+  `__AXI_STREAM_TO_S(__opt_as, __lhs.t, __lhs_sep, __rhs.t, __rhs_sep)    \
+  __opt_as __lhs.tvalid = __rhs.tvalid;
+`define __AXI_STREAM_TO_RSP(__opt_as, __lhs, __lhs_sep, __rhs, __rhs_sep) \
+  __opt_as __lhs.tready = __rhs.tready;
 ////////////////////////////////////////////////////////////////////////////////////////////////////
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////
-// All AXI4-Stream Channels and Request/Response Structs in One Macro
+// Assigning one AXI4 Stream interface to another, as if you would do `assign slv = mst;`
 //
-// This can be used whenever the user is not interested in "precise" control of the naming of the
-// individual channels.
-//
-// Usage Example:
-// `AXI_STREAM_TYPEDEF_ALL(axi_stream, tdata_t, tstrb_t, tkeep_t, tlast_t, tid_t, tdest_t, tuser_t, tready_t)
-//
-// This defines `axi_stream_req_t` and `axi_stream_resp_t` request/response structs
-`define AXI_STREAM_TYPEDEF_ALL(__name, __tdata_t, __tstrb_t, __tkeep_t, __tlast_t, __tid_t, __tdest_t, __tuser_t, __tready_t) \
-  `AXI_STREAM_TYPEDEF_S_CHAN_T(__name``_s_chan_t, __tdata_t, __tstrb_t, __tkeep_t, __tlast_t, __tid_t, __tdest_t, __tuser_t)  \
-  `AXI_STREAM_TYPEDEF_REQ_T(__name``_req_t,__name``_s_chan_t)                                                                 \
-  `AXI_STREAM_TYPEDEF_RSP_T(__name``_rsp_t, __tready_t)
+`define AXI_STREAM_ASSIGN_S(dst, src)          \
+  `__AXI_STREAM_TO_S(assign, dst.t, , src.t, ) \
+  assign dst.tvalid  = src.tvalid;             \
+  assign src.tready  = dst.tready;
+`define AXI_STREAM_ASSIGN(dst, src) \
+  `AXI_STREAM_ASSIGN_S(dst, src)
 ////////////////////////////////////////////////////////////////////////////////////////////////////
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////
-// Flat AXI Stream ports
+// Assigning a stream interface from channel or request/response structs outside a process.
+`define AXI_STREAM_ASSIGN_FROM_S(axi_if, s_struct) `__AXI_STREAM_TO_S(assign, axi_if.t, , s_struct, .)
+`define AXI_STREAM_ASSIGN_FROM_REQ(axi_if, req_struct) `__AXI_STREAM_TO_REQ(assign, axi_if, , req_struct, .)
+`define AXI_STREAM_ASSIGN_FROM_RSP(axi_if, rsp_struct) `__AXI_STREAM_TO_RSP(assign, axi_if, , rsp_struct, .)
+////////////////////////////////////////////////////////////////////////////////////////////////////
+
+////////////////////////////////////////////////////////////////////////////////////////////////////
+// Assigning channel or request/response structs from an interface outside a process.
+`define AXI_STREAM_ASSIGN_TO_R(s_struct, axi_if) `__AXI_STREAM_TO_S(assign, s_struct, ., axi_if.t, )
+`define AXI_STREAM_ASSIGN_TO_REQ(req_struct, axi_if) `__AXI_STREAM_TO_REQ(assign, req_struct, ., axi_if, )
+`define AXI_STREAM_ASSIGN_TO_RSP(rsp_struct, axi_if) `__AXI_STREAM_TO_RSP(assign, rsp_struct, ., axi_if, )
+////////////////////////////////////////////////////////////////////////////////////////////////////
+
+////////////////////////////////////////////////////////////////////////////////////////////////////
+// Macros for assigning flattened AXI stream ports to req/resp AXI Stream structs
 // Vivado naming convention is followed.
-`define AXI_STREAM_FLAT_PORT_TX(__name, __DataWidth, __IdWidth, __DestWidth, __UserWidth) \
-    output logic [__DataWidth-1:0]   __name``_tdata,                                      \
-    output logic [__DataWidth/8-1:0] __name``_tstrb,                                      \
-    output logic [__DataWidth/8-1:0] __name``_tkeep,                                      \
-    output logic                     __name``_tlast,                                      \
-    output logic [__IdWidth-1:0]     __name``_tid,                                        \
-    output logic [__DestWidth-1:0]   __name``_tdest,                                      \
-    output logic [__UserWidth-1:0]   __name``_tuser,                                      \
-    output logic                     __name``_tvalid,                                     \
-    input  logic                     __name``_tready
-`define AXI_STREAM_FLAT_PORT_RX(__name, __DataWidth, __IdWidth, ___DestWidth, __UserWidth) \
-    input   logic [__DataWidth-1:0]   __name``_tdata,                                      \
-    input   logic [__DataWidth/8-1:0] __name``_tstrb,                                      \
-    input   logic [__DataWidth/8-1:0] __name``_tkeep,                                      \
-    input   logic [__Lw-1:0]          __name``_tlast,                                      \
-    input   logic [__IdWidth-1:0]     __name``_tid,                                        \
-    input   logic [__DestWidth-1:0]   __name``_tdest,                                      \
-    input   logic [__UserWidth-1:0]   __name``_tuser,                                      \
-    input   logic                     __name``_tvalid,                                     \
-    output  logic                     __name``_tready
+`define AXI_STREAM_ASSIGN_TO_FLAT(__name, __req, __rsp) \
+  assign __name``_tvalid   = __req.tvalid;              \
+  assign __name``_tdata    = __req.t.data;              \
+  assign __name``_tstrb    = __req.t.strb;              \
+  assign __name``_tkeep    = __req.t.keep;              \
+  assign __name``_tid      = __req.t.id;                \
+  assign __name``_tlast    = __req.t.last;              \
+  assign __name``_tuser    = __req.t.user;              \
+  assign __name``_tdest    = __req.t.dest;              \
+  assign __rsp.tready     = __name``_tready;
+`define AXI_STREAM_ASSIGN_FROM_FLAT(__req, __rsp, __name) \
+  assign __req.tvalid   = __name``_tvalid;                \
+  assign __req.t.data    = __name``_tdata;                \
+  assign __req.t.strb    = __name``_tstrb;                \
+  assign __req.t.keep    = __name``_tkeep;                \
+  assign __req.t.id      = __name``_tid;                  \
+  assign __req.t.last    = __name``_tlast;                \
+  assign __req.t.user    = __name``_tuser;                \
+  assign __req.t.dest    = __name``_tdest;                \
+  assign __name``_tready = __rsp.tready;
 ////////////////////////////////////////////////////////////////////////////////////////////////////
 
 `endif
